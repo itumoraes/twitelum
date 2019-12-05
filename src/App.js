04d3faff0c724fe6699c5eb382b55cea9403f0dd
Italo Moraes Santiago <itu.moraes@gmail.com>
2019-12-04 23:02:30 -0300

    Copia do restante da pasta 02-srcDoProjetoAtualizada/src do projeto arquivos-js46

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
 import Cabecalho from './components/Cabecalho'
-import NavMenu from './components/NavMenu'
-import './App.css'
+import Dashboard from './components/Dashboard'
+import Widget from './components/Widget'
+import TrendsArea from './components/TrendsArea'
+import Tweet from './components/Tweet'
 
 class App extends Component {
   render() {
     return (
-      <Cabecalho>
-        <NavMenu usuario="@omariosouto" />
-      </Cabecalho>
-    )
+      <Fragment>
+        <Cabecalho usuario="@omariosouto" />
+        <div className="container">
+            <Dashboard>
+                <Widget>
+                    <form className="novoTweet">
+                        <div className="novoTweet__editorArea">
+                            <span className="novoTweet__status">0/140</span>
+                            <textarea className="novoTweet__editor" placeholder="O que está acontecendo?"></textarea>
+                        </div>
+                        <button type="submit" className="novoTweet__envia">Tweetar</button>
+                    </form>
+                </Widget>
+                <Widget>
+                    <TrendsArea />
+                </Widget>
+            </Dashboard>
+            <Dashboard posicao="centro">
+                <Widget>
+                    <div className="tweetsArea">
+                        <Tweet />
+                    </div>
+                </Widget>
+            </Dashboard>
+        </div>
+      </Fragment>
+    );
   }
 }
 
-export default App
+export default App;
