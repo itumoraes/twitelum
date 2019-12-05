f4d03ea0df85d08d0a012a2ba5fc8f783589ab75
Italo Moraes Santiago <itu.moraes@gmail.com>
2019-12-04 23:25:30 -0300

    Lógica de criação do tweet estático
    Validação de classes com o state novoTweet

--- src/App.js
+++ src/App.js
@@ -6,32 +6,60 @@ import TrendsArea from './components/TrendsArea'
 import Tweet from './components/Tweet'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      novoTweet: '',
+    }
+  }
+
   render() {
     return (
       <Fragment>
         <Cabecalho usuario="@omariosouto" />
         <div className="container">
-            <Dashboard>
-                <Widget>
-                    <form className="novoTweet">
-                        <div className="novoTweet__editorArea">
-                            <span className="novoTweet__status">0/140</span>
-                            <textarea className="novoTweet__editor" placeholder="O que está acontecendo?"></textarea>
-                        </div>
-                        <button type="submit" className="novoTweet__envia">Tweetar</button>
-                    </form>
-                </Widget>
-                <Widget>
-                    <TrendsArea />
-                </Widget>
-            </Dashboard>
-            <Dashboard posicao="centro">
-                <Widget>
-                    <div className="tweetsArea">
-                        <Tweet />
-                    </div>
-                </Widget>
-            </Dashboard>
+          <Dashboard>
+            <Widget>
+              <form className="novoTweet">
+                <div className="novoTweet__editorArea">
+                  <span className={
+                    `novoTweet__status
+                    ${
+                      this.state.novoTweet.length > 140
+                      ? 'novoTweet__status--invalido'
+                      : ''
+                    }`
+                  }>
+                    { this.state.novoTweet.length }/140
+                  </span>
+                  <textarea
+                    className="novoTweet__editor"
+                    value={ this.state.novoTweet }
+                    onChange={ (event) => this.setState({ novoTweet: event.target.value }) }
+                    placeholder="O que está acontecendo?">    
+                  </textarea>
+                </div>
+                <button
+                  className="novoTweet__envia"
+                  disabled={ this.state.novoTweet.length > 140 || this.state.novoTweet.length === 0 }
+                  type="submit"
+                >
+                  Tweetar
+                </button>
+              </form>
+            </Widget>
+            <Widget>
+              <TrendsArea />
+            </Widget>
+          </Dashboard>
+          <Dashboard posicao="centro">
+            <Widget>
+              <div className="tweetsArea">
+                <Tweet />
+              </div>
+            </Widget>
+          </Dashboard>
         </div>
       </Fragment>
     );
