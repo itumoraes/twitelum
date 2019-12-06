063417964e79e44d16c5c3d3f9f7d5e874bdb612
Italo Moraes Santiago <itu.moraes@gmail.com>
2019-12-06 00:20:04 -0300

    Criação da classe PrivateRoute para validação de usuário logado

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 // Páginas
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 
+class PrivateRoute extends React.Component {
+  estaAutenticado = () => {
+    if (localStorage.getItem('TOKEN')) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  render() {
+    const { component: Component, ...props } = this.props
+
+    if (this.estaAutenticado()) {
+      return <Component {...props} />
+    } else {
+      return <Redirect to="/login" />
+    }
+  }
+}
+
 class Roteamento extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/" component={HomePage} exact />
+        <PrivateRoute path="/" component={HomePage} exact />
         <Route path="/login" component={LoginPage} />
       </Switch>
     )
