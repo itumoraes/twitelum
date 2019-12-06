b023c2158c8a54e6df3378c4f922cbee51b87fce
Italo Moraes Santiago <itu.moraes@gmail.com>
2019-12-05 23:34:03 -0300

    Criação do NotificacaoContext e aplicação teste no submit do formulário de login

--- src/index.js
+++ src/index.js
@@ -18,10 +18,15 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import Roteamento from './routes'
 
+// Context
+import { NotificacaoContextProvider } from './context/NotificacaoContext'
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Roteamento />
-  </BrowserRouter>,
+  <NotificacaoContextProvider>
+    <BrowserRouter>
+      <Roteamento />
+    </BrowserRouter>
+  </NotificacaoContextProvider>,
   document.getElementById('root')
 );
 
